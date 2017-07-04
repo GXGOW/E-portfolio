3939fa7ad37e73816b8a4a7e72b2236f90b8b90c
Nicolas Loots <nicolas.loots@outlook.be>
2017-07-04 12:49:16 +0200

    Foutieve vertaling bijgewerkt

--- locale/nl_BE/trump.php
+++ locale/nl_BE/trump.php
@@ -6,7 +6,6 @@
  * Time: 23:11
  */
 $translated = array(
-    'p1' => 'Click on Trump to make him talk',
-    'p2' => 'In case you\'re sick of seeing his face, you can click <a href="portfolio.php">here</a> to return to the 
-    previous page.'
+    'p1' => 'Klik op Trump om hem te doen praten',
+    'p2' => 'Als je Trump beu bent gezien, kan je <a href="portfolio.php">terugkeren naar de vorige pagina</a>.'
 );
