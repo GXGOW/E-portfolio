ffb11f79618234fadb2b83321ba40be117c7b68f
Nicolas Loots <nicolas.loots@outlook.be>
2017-08-17 13:32:21 +0200

    Origineel stylesheet en script toegevoegd

--- html/trump.php
+++ html/trump.php
@@ -9,12 +9,10 @@ global $translated; ?>
     <meta charset="UTF-8">
     <link rel="icon" href="../images/trumpface.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=yes">
-    <!-- Android Chrome colored statusbar -->
     <meta name="theme-color" content="#187ab2">
-    <!-- iOS colored statusbar -->
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="#187ab2">
-    <link href="../build/trump/trump.min.css" rel="stylesheet">
+    <link href="../build/trump/trump.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
     <title>Trump</title>
 </head>
@@ -25,6 +23,6 @@ global $translated; ?>
     <img src="../images/sadtrump.jpg" id="trump" alt="Mad Trump"/>
             <audio id="audio" src="" />
         </div>
-<script src="../build/trump/trump.min.js"></script>
+<script src="../build/trump/trump.js"></script>
 </body>
 </html>
