71b0063719f0ff29ae6943f5e428a42442df7803
Nicolas Loots <nicolas.loots@outlook.be>
2017-09-19 12:53:20 +0200

    Documentatie aan algoritme voor genereren van tabel toegevoegd

--- html/cv.php
+++ html/cv.php
@@ -1,34 +1,45 @@
 <?php
 include '../php/functions.php';
-include '../locale/' . $_SESSION["lang"] . '/' . basename($_SERVER["PHP_SELF"]);
-global $translated; ?>
-        <div id="cv">
-            <?php
-            //Die tabellen invoegen was nog wel knap lastig
-            foreach ($translated as $item) {
-                echo '<div class="tablepart">';
-                foreach ($item as $keypart => $part) {
-                    if ($keypart == 'h') {
-                        echo '<h3>' . $part . '</h3>';
+include '../locale/' . $_SESSION["lang"] . '/' . basename($_SERVER["PHP_SELF"]);?>
+<div id="cv">
+    <?php
+    //Array opsplitsen in verschillende delen van de tabel
+    foreach ($translated as $item) {
+        //Begin tablepart
+        echo '<div class="tablepart">';
+        //Delen van tablepart overlopen
+        foreach ($item as $keypart => $part) {
+            //Header invoegen
+            if ($keypart == 'h') {
+                echo '<h3>' . $part . '</h3>';
+            } else {
+                //Begin tabel
+                echo '<table>';
+                //Delen tabel overlopen
+                foreach ($part as $tablekey => $table) {
+                    //Begin tablerow
+                    echo '<tr>';
+                    //Aangepaste rowspan invoegen volgens naamgeving
+                    if (strpos($tablekey, '_r')) {
+                        $rowspan = substr($tablekey, -1);
+                        echo '<th rowspan="' . $rowspan . '">' . substr($tablekey, 0, -3) . '</th>
+                                <td>' . $table . '</td>';
+                    //Table definition zonder table header invoegen volgens naamgeving
+                    } else if (strpos($tablekey, '_td') === 0) {
+                        echo '<td>' . $table . '</td>';
+                    //Normale table header + table definition combinatie invoegen
                     } else {
-                        echo '<table>';
-                        foreach ($part as $tablekey => $table) {
-                            echo '<tr>';
-                            if (strpos($tablekey, '_r')) {
-                                $rowspan = substr($tablekey, -1);
-                                echo '<th rowspan="' . $rowspan . '">' . substr($tablekey, 0, -3) . '</th>
-                                        <td>' . $table . '</td>';
-                            } else if (strpos($tablekey, '_td') === 0) {
-                                echo '<td>' . $table . '</td>';
-                            } else {
-                                echo '<th>' . $tablekey . '</th><td>' . $table . '</td>';
-                            }
-                            echo '</tr>';
-                        }
+                        echo '<th>' . $tablekey . '</th><td>' . $table . '</td>';
                     }
-                    echo '</table>';
+                    //Einde tablerow
+                    echo '</tr>';
                 }
-                echo '</div>';
             }
-            ?>
-        </div>
+            //Einde tabel
+            echo '</table>';
+        }
+        //Einde tablepart
+        echo '</div>';
+    }
+    ?>
+</div>
