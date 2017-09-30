d587cdb9b3b132a6c0a770882e3514c64e610d14
Nicolas Loots <nicolas.loots@outlook.be>
2017-09-30 14:23:33 +0200

    Implementatie Google Analytics

--- php/functions.php
+++ php/functions.php
@@ -68,6 +68,14 @@ function getScripts()
             }
         })});
         </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-91503104-2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)};
+          gtag("js", new Date());
+          gtag("config", "UA-91503104-2");
+        </script>
+        
         ';
     echo $scripts;
 }
