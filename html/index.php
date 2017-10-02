e28d2ae0922bcc0e0914210c14c17df39e905d58
Nicolas Loots <nicolas.loots@outlook.be>
2017-10-02 17:43:47 +0200

    Reddit-poem aangepast

--- html/index.php
+++ html/index.php
@@ -1,23 +1,19 @@
 <?php
 include '../php/functions.php';
 include '../locale/' . $_SESSION["lang"] . '/' . basename($_SERVER["PHP_SELF"]);?>
-<?php initSlides('../images/slideshow') ?>
-<div id="content">
-    <article>
-        <?php
+    <?php initSlides('../images/slideshow') ?>
+    <div id="content">
+        <article>
+            <?php
         foreach ($translated as $item) {
             echo '<p style="text-align: center">' . $item . '</p>';
         }
         ?>
-    </article>
-    <div class="iframe">
-        <div class="reddit-embed" data-embed-media="www.redditmedia.com" data-embed-parent="false"
-             data-embed-live="true" data-embed-uuid="30bb254b-4ced-4793-b78c-6aebe321a8d5"
-             data-embed-created="2017-05-25T11:33:08.463Z"><a
-                    href="https://www.reddit.com/r/movies/comments/2da3m3/robin_williams_dead_at_63/cjnkwxq/">Comment</a>
-            from discussion <a href="https://www.reddit.com/r/movies/comments/2da3m3/robin_williams_dead_at_63/">Robin
-                Williams dead at 63</a>.
+        </article>
+        <div class="iframe">
+            <div class="reddit-embed" data-embed-media="www.redditmedia.com" data-embed-parent="false" data-embed-live="false" data-embed-uuid="5b778ff5-9726-48f9-a4f3-38cc4efcdda5" data-embed-created="2017-09-30T16:33:44.885Z">
+                <a href="https://www.reddit.com/r/AskReddit/comments/724wbv/how_do_you_reply_to_how_much_do_you_love_me/dnfsgu5/">Comment</a> from discussion
+                <a href="https://www.reddit.com/r/AskReddit/comments/724wbv/how_do_you_reply_to_how_much_do_you_love_me/">How do you reply to &quot;how much do you love me&quot;?</a>.</div>
         </div>
     </div>
-</div>
-<script async src="https://www.redditstatic.com/comment-embed.js"></script>
+    <script async src="https://www.redditstatic.com/comment-embed.js"></script>
