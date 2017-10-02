53f7e885c87f465e96025363ea53acff6917c39d
Nicolas Loots <nicolas.loots@outlook.be>
2017-10-02 17:53:13 +0200

    Overbodige vertalingen verwijderd

--- locale/en_GB/errors.php
+++
@@ -1,8 +0,0 @@
-<?php
-$errors = array(
-    'required' => 'Please fill in this field',
-    'minlengthLN' => 'Please enter a valid name',
-    'minlengthFN' => 'Please enter a valid first name',
-    'email' => 'Please enter a valid email address',
-    'captcha' => 'Please verify the captcha'
-);
--- locale/nl_BE/errors.php
+++
@@ -1,8 +0,0 @@
-<?php
-$errors = array(
-    'required' => 'Gelieve dit veld in te vullen',
-    'minlengthLN' => 'Gelieve een geldige naam in te vullen',
-    'minlengthFN' => 'Gelieve een geldige voornaam in te vullen',
-    'email' => 'Gelieve een geldig e-mailadres in te vullen',
-    'captcha' => 'Gelieve de captcha aan te vinken'
-);
