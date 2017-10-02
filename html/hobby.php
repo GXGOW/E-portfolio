fa9be58831bad60f662e3d462f8fb75b9bbfc587
Nicolas Loots <nicolas.loots@outlook.be>
2017-10-02 20:52:29 +0200

    Playlist bijgewerkt

--- html/hobby.php
+++ html/hobby.php
@@ -13,8 +13,7 @@ include '../locale/' . $_SESSION["lang"] . '/' . basename($_SERVER["PHP_SELF"]);
                 break;
             case $key === 'video':
                 echo '<div class="iframe">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/5ItHNdrPEh0?list=RD5ItHNdrPEh0"
-                frameborder="0" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/8i8uLP71U8Q" frameborder="0" allowfullscreen></iframe>
     </div>';break;
             case $key === 'image':
                 echo '<picture><img src="../images/klj.jpg" alt="KLJ"></picture>';
